0db26896afbe32aa2da1629c0cb15989ab86d921
williambl <william@williambl.com>
2026-09-10 14:01:52 +0100

    Switch to using new YUNG's API LocateReplacer API
    YUNG-GANG/YUNGs-Better-Desert-Temples#51

--- Common/src/main/java/com/yungnickyoung/minecraft/betterwitchhuts/BetterWitchHutsCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterwitchhuts/BetterWitchHutsCommon.java
@@ -3,6 +3,11 @@ package com.yungnickyoung.minecraft.betterwitchhuts;
 import com.yungnickyoung.minecraft.betterwitchhuts.module.ConfigModule;
 import com.yungnickyoung.minecraft.betterwitchhuts.services.Services;
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
+import com.yungnickyoung.minecraft.yungsapi.api.world.structure.locate.LocateReplacer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,5 +19,9 @@ public class BetterWitchHutsCommon {
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.betterwitchhuts.module");
         Services.MODULES.loadModules();
+
+        LocateReplacer.register(BuiltinStructures.SWAMP_HUT,
+                ResourceKey.create(Registries.STRUCTURE, Identifier.fromNamespaceAndPath(MOD_ID, "witch_hut")),
+                () -> CONFIG.general.disableVanillaWitchHuts);
     }
 }
